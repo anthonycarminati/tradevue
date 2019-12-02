a3e9a2e054e376c115d462405d82db079a233836
Anthony Carminati <anthonycarminati@anthonys-mbp-2.myfiosgateway.com>
2019-12-02 12:30:10 -0500

    checkpoint

--- src/App.vue
+++ src/App.vue
@@ -3,33 +3,10 @@
         <v-app-bar app color="primary" dark>
             <v-toolbar-title>Tradevue</v-toolbar-title>
             <v-spacer/>
-            <v-btn v-for="link in links" :key="link" text rounded>{{link}}</v-btn>
+            <v-btn v-for="link in links" :key="link.label" text rounded :to="link.url">{{link.label}}</v-btn>
         </v-app-bar>
         <v-content>
-            <!--Login Card-->
-            <v-card width="400px" class="mx-auto mt-5">
-                <v-card-title>
-                    <h1> Hello World! </h1>
-                </v-card-title>
-                <v-card-text>
-                    <v-form>
-                        <v-text-field label="Username" prepend-icon="mdi-account-circle"/>
-                        <v-text-field
-                                label="Password"
-                                :type="showPassword ? 'text' : 'password'"
-                                prepend-icon="mdi-lock"
-                                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                                @click:append="showPassword = !showPassword"
-                        />
-                    </v-form>
-                </v-card-text>
-                <v-divider/>
-                <v-card-actions>
-                    <v-btn color="success">Register</v-btn>
-                    <v-spacer/>
-                    <v-btn color="info">Login</v-btn>
-                </v-card-actions>
-            </v-card>
+          <router-view/>
         </v-content>
     </v-app>
 </template>
@@ -37,15 +14,20 @@
 <script>
     export default {
         name: 'App',
-
-        components: {
-        },
-
         data: () => ({
-            showPassword: false,
             links: [
-                'Home',
-                'Login'
+              {
+                label: 'Home',
+                url: '/'
+              },
+              {
+                label: 'Login',
+                url: '/login'
+              },
+              {
+                label: 'Dashboard',
+                url: '/dashboard'
+              }
             ]
         }),
     };
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>DAsh</h1>
+</template>
+
+<script>
+    export default {
+        name: 'DashboardPage',
+        data: () => ({
+            //
+        })
+    };
+</script>
+
+<style>
+
+</style>
