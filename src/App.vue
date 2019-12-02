c358462d8bfc7b2f3a389bad960b9eee7eb27ae8
Anthony Carminati <anthonycarminati@anthonys-mbp-2.myfiosgateway.com>
2019-12-02 14:44:31 -0500

    added new view plus formatting

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,45 @@
 <template>
-    <v-app>
-        <v-app-bar app color="primary" dark>
-            <v-toolbar-title>Tradevue</v-toolbar-title>
-            <v-spacer/>
-            <v-btn v-for="link in links" :key="link.label" text rounded :to="link.url">{{link.label}}</v-btn>
-        </v-app-bar>
-        <v-content>
-          <router-view/>
-        </v-content>
-    </v-app>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Tradevue</v-toolbar-title>
+      <v-spacer />
+      <v-btn
+        v-for="link in links"
+        :key="link.label"
+        text
+        rounded
+        :to="link.url"
+        >{{ link.label }}</v-btn
+      >
+    </v-app-bar>
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-    export default {
-        name: 'App',
-        data: () => ({
-            links: [
-              {
-                label: 'Home',
-                url: '/'
-              },
-              {
-                label: 'Login',
-                url: '/login'
-              },
-              {
-                label: 'Dashboard',
-                url: '/dashboard'
-              }
-            ]
-        }),
-    };
+export default {
+  name: "App",
+  data: () => ({
+    links: [
+      {
+        label: "Home",
+        url: "/"
+      },
+      {
+        label: "Login",
+        url: "/login"
+      },
+      {
+        label: "Dashboard",
+        url: "/dashboard"
+      },
+      {
+        label: "Signup",
+        url: "/signup"
+      }
+    ]
+  })
+};
 </script>
